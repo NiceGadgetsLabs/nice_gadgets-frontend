fce3a8336507273cc1986c2a22aad5608cd8dc52
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-06-07 19:28:13 +0300

    feat: add project structure and page stubs

---
+++ src/pages/CatalogPage.tsx
@@ -0,0 +1,3 @@
+export const CatalogPage = () => {
+  return <h1>CatalogPage</h1>;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import svgr from 'vite-plugin-svgr';
 
 /** @type {import('vite').UserConfig} */
 export default {
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), svgr()],
   resolve: {
     alias: { '@': '/src' },
   },
