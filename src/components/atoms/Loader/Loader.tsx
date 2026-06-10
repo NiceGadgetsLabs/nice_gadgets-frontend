37bb32e260d39ff86266d2a223ce3460f3a46b1c
Andrey <andreiverpeka123@gmail.com>
2026-06-10 10:12:28 +0300

    fix: add type for Loader and fixed fixed sizes

--- src/components/atoms/Loader/Loader.tsx
+++ src/components/atoms/Loader/Loader.tsx
@@ -1,9 +1,20 @@
 import './Loader.scss';
 
-export const Loader = () => {
+type LoaderProps = {
+  size?: number;
+};
+
+export const Loader = ({ size = 40 }: LoaderProps) => {
   return (
     <div className="loader">
-      <div className="loader__spinner" />
+      <div
+        className="loader__spinner"
+        style={{
+          width: size,
+          height: size,
+          borderWidth: size / 10,
+        }}
+      />
     </div>
   );
 };
