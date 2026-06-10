2e4e5a16b6a14ea6ac78f985d407c8e8e6f14b75
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-06-10 12:29:02 +0300

    feat: improve state management and styling

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,19 +1,17 @@
 import { Outlet } from 'react-router-dom';
-import { useState } from 'react';
 import { Header } from '../../components/organisms/Header/Header';
-import { BurgerMenu } from '../../components/organisms/BurgerMenu/BurgerMenu';
 import { Footer } from '../../components/organisms/Footer';
+import { Container } from '../../components/atoms/Container/Container';
 import './MainLayout.scss';
 
 export const MainLayout = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   return (
     <div className="layout">
-      <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-      {isMenuOpen && <BurgerMenu setIsMenuOpen={setIsMenuOpen} />}
+      <Header />
       <main className="main">
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </main>
       <Footer />
     </div>
