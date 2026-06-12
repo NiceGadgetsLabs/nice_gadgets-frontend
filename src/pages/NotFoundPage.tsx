029e1f4aa205a0d4267dc6f3a5466e16f0294f1c
Viktor Gulko <viq@ukr.net>
2026-06-12 12:08:37 +0300

    feat: added initial changes

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,3 +1,21 @@
+import k2Image from '../assets/images/K2.png';
+
 export const NotFoundPage = () => {
-  return <h1>NotFoundPage</h1>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${k2Image})`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {/* <h1>NotFoundPage</h1> */}
+    </div>
+  );
 };
