40ffbf4a415bb08f184806b3b4452ebb0164f8bd
Serhii Beskrovnyi <wpserggun@gmail.com>
2026-06-13 23:45:58 +0300

    chore: add new configuration for favorites, cart and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-import { CartProvider } from './contexts/cart/CartProvider';
-import { FavoritesProvider } from './contexts/favorites/FavoritesProvider';
+import { Navigate, Routes, Route } from 'react-router-dom';
+import { MainLayout } from './layouts/MainLayout/MainLayout';
+import { HomePage } from './pages/HomePage/HomePage';
+import type { Product } from './types/Products';
+import { useContext, useEffect, useState } from 'react';
+
+import { getProductAll } from './services/api';
+
+import { FavoritesContext } from './contexts/favorites/FavoritesContext';
+import { CartPage } from './pages/CartPage/CartPage';
+import { FavoritesPage } from './pages/FavoritesPage/FavoritesPage';
 
 export const App = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const { fav } = useContext(FavoritesContext);
+
+  useEffect(() => {
+    getProductAll()
+      .then((data: Product[]) => {
+        setProducts(data);
+      })
+      .catch(() => {
+        alert('Failed to load products');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <CartProvider>
-      <FavoritesProvider>
-        <RouterProvider router={router} />
-      </FavoritesProvider>
-    </CartProvider>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage products={products} loading={loading} />} />
+        <Route path="favorites" element={<FavoritesPage products={fav} />} />
+        <Route path="favourites" element={<Navigate to="/favorites" replace />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="*" element={<HomePage products={products} loading={loading} />} />
+      </Route>
+    </Routes>
   );
 };
