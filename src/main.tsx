1ce5b3956258336ff2288d42a007ab200e463be3
Serhii Beskrovnyi <wpserggun@gmail.com>
2026-06-13 23:49:45 +0300

    chore: add new configuration for favorites, cart, home page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.scss';
+import { HashRouter as Router } from 'react-router-dom';
+import { FavoritesProvider } from './contexts/favorites/FavoritesProvider.tsx';
+import { CartProvider } from './contexts/cart/CartProvider.tsx';
+import { ScrollToTop } from './services/ToTop.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <ScrollToTop>
+        <FavoritesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FavoritesProvider>
+      </ScrollToTop>
+    </Router>
   </StrictMode>,
 );
